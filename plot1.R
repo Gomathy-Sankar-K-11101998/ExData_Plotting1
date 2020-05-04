d9afaf6c5b00bd4789ef88556a06bf23473d30cd
Gomathy-Sankar-K-11101998 <gomathysankar_krishnan@srmuniv.edu.in>
2020-05-05 01:33:09 +0530

    The file for plotting plot1.ong s performed successfullly.

---
+++ plot1.R
@@ -0,0 +1,8 @@
+house_power_consume = read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+
+house_power_consume = transform(house_power_consume, Date = as.Date(Date, "%d/%m/%Y"))
+house_power_consume = subset(house_power_consume, Date == "2007-02-02" | Date == "2007-02-01")
+house_power_consume = transform(house_power_consume, Global_active_power = as.numeric(Global_active_power))
+hist(house_power_consume$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+dev.copy(png, file = "plot1.png")
+dev.off()

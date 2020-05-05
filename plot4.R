3d961dd239f8fea2d62cfa277fa7bda3d82a5ef4
Gomathy-Sankar-K-11101998 <gomathysankar_krishnan@srmuniv.edu.in>
2020-05-05 21:54:40 +0530

    legend function is changed in plot 4 function.

--- plot4.R
+++ plot4.R
@@ -20,9 +20,9 @@ with(house_power_consume, plot(Time, Voltage, type = "l", xlab = "datetime", yla
 with(house_power_consume, plot(Time, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
 with(house_power_consume, lines(Time, Sub_metering_2, type = "l", col = "red"))
 with(house_power_consume, lines(Time, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, y.intersp = 0.2, text.font = 1, cex = 0.4, fill = "transparent", border = "white")
+legend("topright", lty=1, adj = c(0,0), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", y.intersp = 0.2, x.intersp = 0.25, text.width = strwidth("Sub_meteri"), xjust = 1, seg.len = 1, title.adj = 0)
 
-with(house_power_consume, plot(Time, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power", lty = par(0.5)))
+with(house_power_consume, plot(Time, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power", lwd = 0.5))
 
 dev.copy(png, file = "plot4.png")
 dev.off()
